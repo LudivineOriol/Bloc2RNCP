49a3f716c6b58ba8ece5c765b284544dbc8d30c0
Oriol <oriol.ludivine@gmail.com>
2026-07-10 22:27:13 +0200

    Docker MySQL configuré et fonctionnel (port 3307), Connexion Flask ↔ MySQL testée et validée

--- app.py
+++ app.py
@@ -12,14 +12,19 @@ app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT'))
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 mysql = MySQL(app)
 
 # Route de test
-@app.route('/')
-def index():
-    return jsonify({"message": "API Pâtissons opérationnelle"})
+@app.route('/test-db')
+def test_db():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT DATABASE();')
+    db_name = cur.fetchone()
+    cur.close()
+    return jsonify({"connexion": "réussie", "base": db_name[0]})
 
 if __name__ == '__main__':
     app.run(debug=True)
